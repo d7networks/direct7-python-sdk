e7d54e5ec93fb30cff1a607b4dd2e6193d53649d
sisira <sisira@d7networks.com>
2024-03-14 09:29:04 +0530

    whatsapp meta

--- main.py
+++ main.py
@@ -3,8 +3,5 @@ from direct7 import Client
 if __name__ == "__main__":
     client = Client(api_token="Your API Token")
 
-result = client.sms.send_message({"recipients": ["+9199XXXXXXXX"], "content": "Greetings from D7 API", "unicode": False},
-                                 originator="Sender",
-                                 report_url="https://the_url_to_receive_delivery_report.com",
-                                 )
+result = client.whatsapp.send_whatsapp_freeform_message(originator="XXXXXXXXXXX", recipient=["XXXXXXXXXXX", "XXXXXXXXXXXXX"], message_type="CONTACTS", first_name="Amal", last_name="Anu", formatted_name="Amal Anu", phones=["918086757074", "917306445534"], emails = ["amal@gmail.com", "amal@gmail1.com"])
 print(result)
